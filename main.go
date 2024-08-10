13195f3eef923491e7cc7032882893c885a9b147
bcm <limit9895@naver.com>
2024-08-10 13:41:44 +0900

    test페이지 추가

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"log"
+	"sync"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+var counter int
+var mu sync.Mutex
+
+type Response struct {
+	Value int `json:"value"`
+}
 
 func main() {
 	// Gin 라우터 설정
@@ -17,13 +25,33 @@ func main() {
 	// CORS 설정
 	router.Use(cors.Default())
 
-	// 정적 파일 서버 설정 (React 빌드 파일 서빙), 즉 프론트앤드 동작시 static으로 들어오는 내용에 대해 해당 경로로 안내
+	// 정적 파일 서버 설정 (React 빌드 파일 서빙)
 	router.Static("/static", "../frontend/build/static")
+
+	// API 엔드포인트 설정
+	router.GET("/increment", func(c *gin.Context) {
+		mu.Lock()
+		counter++
+		value := counter
+		mu.Unlock()
+
+		c.JSON(200, Response{Value: value})
+	})
+
+	router.GET("/decrement", func(c *gin.Context) {
+		mu.Lock()
+		counter--
+		value := counter
+		mu.Unlock()
+
+		c.JSON(200, Response{Value: value})
+	})
 
 	// 모든 경로에서 React 빌드된 파일 서빙
 	router.NoRoute(func(c *gin.Context) {
 		c.File("../frontend/build/index.html")
 	})
+
 	// 서버 실행
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("서버 실행 실패: %v", err)
